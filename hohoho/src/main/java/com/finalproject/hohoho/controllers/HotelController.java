c5bafe2c9d1c51c999577831e658e2b976ab0ef9
Sergi Rodriguez Utge <sergidpm@hotmail.com>
2023-12-08 19:30:04 +0100

    base to filter and implements pagination

--- hohoho/src/main/java/com/finalproject/hohoho/controllers/HotelController.java
+++ hohoho/src/main/java/com/finalproject/hohoho/controllers/HotelController.java
@@ -1,10 +1,12 @@
 package com.finalproject.hohoho.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.finalproject.hohoho.dto.Hotel;
+import com.finalproject.hohoho.dto.Town;
 import com.finalproject.hohoho.services.HotelServiceImpl;
 
 @RestController
@@ -14,18 +16,28 @@ public class HotelController {
 	@Autowired
 	HotelServiceImpl hotelServiceImpl;
 
-	// Get all hotels
-	@GetMapping("/hotels")
-	public List<Hotel> list() {
+	// Get paginated all hotels from location
+	@GetMapping("/hotels/{idTown}")
+	public List<Hotel> list(@RequestParam(name = "idTown", required = false) Integer idTown,
+			@RequestParam(name = "minStarRatingAvg", required = false) Integer minStarRatingAvg,
+			@RequestParam(name = "numberRooms", required = false) Integer numberRooms,
+			@RequestParam(name = "minPrice", required = false) Integer minPrice,
+			@RequestParam(name = "maxPrice", required = false) Integer maxPrice,
+			@RequestParam(name = "idService", required = false) List<Integer> idService) {
+
+		// Data declaration
+		List<Hotel> allHotels = new ArrayList<Hotel>();
+		boolean isFiltered = false;
+
 		return hotelServiceImpl.list();
 	}
-	
+
 	// Add new hotel
 	@PostMapping("/hotel/add")
 	public Hotel save(@RequestBody Hotel hotel) {
 		return hotelServiceImpl.save(hotel);
 	}
-	
+
 	// Get hotel by id
 	@GetMapping("/hotel/{id}")
 	public Hotel byId(@PathVariable(name = "id") Integer id) {
@@ -33,7 +45,7 @@ public class HotelController {
 		hotelByID = hotelServiceImpl.byId(id);
 		return hotelByID;
 	}
-	
+
 	// Update hotel by id
 	@PutMapping("/hotel/update/{id}")
 	public Hotel update(@PathVariable(name = "id") Integer id, @RequestBody Hotel hotel) {
@@ -59,7 +71,7 @@ public class HotelController {
 
 		return hotelUpdated;
 	}
-	
+
 	// Delete hotel by id
 	@DeleteMapping("/hotel/delete/{id}")
 	public void delete(@PathVariable Integer id) {
