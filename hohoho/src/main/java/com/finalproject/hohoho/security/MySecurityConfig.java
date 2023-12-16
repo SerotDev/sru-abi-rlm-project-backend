20717cbafcf3cf789e66fef76aa3a3f3310dea89
robertiki2001 <robertrlm16@gmail.com>
2023-12-16 19:57:57 +0100

    Cors security

--- hohoho/src/main/java/com/finalproject/hohoho/security/MySecurityConfig.java
+++ hohoho/src/main/java/com/finalproject/hohoho/security/MySecurityConfig.java
@@ -1,5 +1,7 @@
 package com.finalproject.hohoho.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +29,68 @@ import com.finalproject.hohoho.security.service.UserDetailsServiceImpl;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class MySecurityConfig {
+	
+	private static final String[] USER_SECURED_URLs = {
+			"/api/user/favouriteHotels/{userId}", 
+			"/api/addFavourites", "/api/addFavourite/add", 
+			"/api/addFavourite/{id}", 
+			"/api/addFavourite/update/{id}", 
+			"/api/addFavourite/delete/{id}"
+			};
+	
+	private static final String[] HOTEL_SECURED_URLs = {
+			"/api/user/hotels/{idUser}",
+			"/api/user/events/{idHotel}", 
+			"/api/event/add}",
+			"/api/event/update/{id}",
+			"/api/event/delete/{id}",
+			"/api/hotel/add",
+			"/api/hotel/update/{id}",
+			"/api/hotel/delete/{id}",
+			"/api/hotelService/add",
+			"/api/hotelService/update/{id}",
+			"/api/hotelService/delete/{id}"
+			};
+	
+	private static final String[] ADMIN_SECURED_URLs = {
+			"/api/users", 
+			"/api/user/add", 
+			"/api/user/{id}", 
+			"/api/user/update/{id}",
+			"/api/roles",
+			"/api/role/add",
+			"/api/role/{id}",
+			"/api/role/update/{id}",
+			"/api/role/delete/{id}",
+			"/api/service/add",
+			"/api/service/update/{id}",
+			"/api/service/delete/{id}",
+			"/api/towns",
+			"/api/town/add",
+			"/api/town/{id}",
+			"/api/town/update/{id}",
+			"/api/town/delete/{id}",
+    		"/api/user/delete/{id}"
+			};
 
-    @Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler;
+    private static final String[] ALLOW_GET_URLs = {
+            "/api/hotels/{page}{size}{idTown}{search}{minStarRatingAvg}{minNumberRooms}{minPrice}{maxPrice}{idServices}",
+            "/api/hotel/{id}",
+            "/api/hotel/starRatingAvg/{hotelId}",
+            "/api/events",
+            "/api/events-private/{idHotel}",
+            "/api/events-public/{idTown}",
+            "/api/event/{id}",
+            "/api/hotelServices",
+            "/api/hotelServices/{id}",         
+            "/api/swagger-ui/**",
+            "/api/swagger-resources/*",
+            "/api/v3/api-docs/**",
+            "/api/services",
+            "/api/service/{id}"
+    };
+
+    public static final String[] ALLOW_POST_URLs = {"/login" ,"/register"};
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
@@ -61,33 +122,34 @@ public class MySecurityConfig {
         auth.userDetailsService(this.userDetailsService).passwordEncoder(passwordEncoder());
     }
     
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+         CorsConfiguration configuration = new CorsConfiguration();
+         configuration.addAllowedOrigin("https://sru-abi-rlm-project-backend-production.up.railway.app/swagger-ui/index.html#/**");
+         configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT"));
+         configuration.addAllowedHeader("*");
+         configuration.setAllowCredentials(true);
+         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+         source.registerCorsConfiguration("/**", configuration);
+         return source;
+     }
+
 	 @Bean
-	 CorsConfigurationSource corsConfigurationSource() {
-	    	
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.addAllowedOrigin("*");      // Allow all origins or Arrays.asList("http://localhost:4200","http://localhost:3000")
-	        configuration.addAllowedMethod("*");      // Allow all methods or List.of("GET", "POST", "PUT", "DELETE")
-	        configuration.addAllowedHeader("*");      // Allow all headers
-	        configuration.setAllowCredentials(true);  // Allow sending of authentication cookies
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
+	 SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        return http.csrf(csrf -> csrf.disable())
+	        		.cors(cors -> cors.configurationSource(corsConfigurationSource())).authorizeHttpRequests(auth-> auth
+	        				.requestMatchers(HttpMethod.GET, ALLOW_GET_URLs).permitAll()
+	                        .requestMatchers(HttpMethod.POST,ALLOW_POST_URLs).permitAll()
+	                        .requestMatchers(USER_SECURED_URLs).hasAuthority("ADMIN, VISITOR")
+	                        .requestMatchers(HOTEL_SECURED_URLs).hasAuthority("ADMIN, HOTEL")
+	                        .requestMatchers(ADMIN_SECURED_URLs).hasAuthority("ADMIN")
+	                        .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+	                        )
+	                .sessionManagement(management -> management
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	                .authenticationProvider(authenticationProvider())
+	                .addFilterBefore(authorizationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+	                .build();
 	    }
-
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http ,  AuthenticationManager authenticationManager) throws Exception {
-
-      return  http.cors(cors -> corsConfigurationSource())                            
-        .csrf(csrf -> csrf.disable())                                                            
-        .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandler))
-        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests( auth -> auth
-        	.requestMatchers("**").permitAll()
-        	.requestMatchers(HttpMethod.OPTIONS).permitAll()
-        	.anyRequest().authenticated()
-        )
-        	.authenticationProvider(authenticationProvider())
-            .addFilterBefore(authorizationJwtTokenFilter(),UsernamePasswordAuthenticationFilter.class)
-            .build();
-	}
 }
