abf5cd7afb7b086cc03ec7f5a1d29bd0ec3ea91a
robertiki2001 <robertrlm16@gmail.com>
2023-12-20 00:20:58 +0100

    Commit login body response with rol and username

--- hohoho/src/main/java/com/finalproject/hohoho/jwt/JWTController.java
+++ hohoho/src/main/java/com/finalproject/hohoho/jwt/JWTController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalproject.hohoho.exceptions.UserNotFoundException;
+import com.finalproject.hohoho.security.service.UserDetailsServiceImpl;
+
 import org.json.JSONObject;
 
 /**
@@ -27,17 +30,20 @@ public class JWTController {
     private JWTService jwtService;
 	@Autowired
     private AuthenticationManager authenticationManager;
+	@Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @PostMapping
     public Object getTokenForAuthenticatedUser(@RequestBody JWTAuthenticationRequest authRequest){
-    	System.out.println(authRequest.getUsername());
-    	System.out.println(authRequest.getPassword());
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         if (authentication.isAuthenticated()){
             String token =  jwtService.generateToken(authRequest.getUsername());
-            JSONObject jsonObject = new JSONObject("{\"token\": \"" + token + "\"}");
-            jsonObject.put("token",token );
+            UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("token",token);
+            jsonObject.put("username",authRequest.getUsername());
+            jsonObject.put("rol",userDetails.getAuthorities());
             return jsonObject.toMap();//devuelve token por body
         }
         else {
